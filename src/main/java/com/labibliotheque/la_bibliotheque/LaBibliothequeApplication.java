57806f3a62b32612176140152c2a405014f941a2
Fred-18 <fred.nobre@octo.com>
2023-03-16 14:36:14 +0100

    first commit

---
+++ src/main/java/com/labibliotheque/la_bibliotheque/LaBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.labibliotheque.la_bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBibliothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaBibliothequeApplication.class, args);
+    }
+
+}
