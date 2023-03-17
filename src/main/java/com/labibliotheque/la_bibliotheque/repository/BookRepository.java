0b244e7bc36062e5a8919378969facf280b186d1
anharaAkter <anhara.susoma@gmail.com>
2023-03-17 15:09:33 +0100

    🗃️ : added repository for the correspondence classes

---
+++ src/main/java/com/labibliotheque/la_bibliotheque/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.labibliotheque.la_bibliotheque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository {
+}
---
+++ src/main/java/com/labibliotheque/la_bibliotheque/repository/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.labibliotheque.la_bibliotheque.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends CrudRepository {
+}
