affcc7096e16bdb034f3796ef8cd7c4f9c88dabe
anharaAkter <anhara.susoma@gmail.com>
2023-03-20 18:17:09 +0100

    🗃️ : added foreign key to table comments

--- src/main/java/com/labibliotheque/la_bibliotheque/model/Books.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/model/Books.java
@@ -1,7 +1,9 @@
 package com.labibliotheque.la_bibliotheque.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,10 +12,12 @@ import java.util.Date;
 @Entity
 public class Books {
     @Id
+    @Column(name = "id_book")
     private int idBook;
     private String title;
     private String description;
     private boolean availability;
     private Date publishDate;
     private int rating;
+
 }
