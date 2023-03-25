1c935d23f884b2ac6fc97bac867b931d461c5ed3
Fred-18 <fred.nobre@octo.com>
2023-03-25 12:21:03 +0100

    :wrench: add user service

---
+++ src/main/java/com/labibliotheque/la_bibliotheque/services/UserService.java
@@ -0,0 +1,27 @@
+package com.labibliotheque.la_bibliotheque.services;
+
+import com.labibliotheque.la_bibliotheque.models.User;
+import com.labibliotheque.la_bibliotheque.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository us;
+    
+    public Optional<User>getUser(int id){
+        return us .findById(id);
+    }
+    public Iterable<User>getAllUsers(){
+        return us.findAll();
+    }
+    public void addUser(User user){
+        us.save(user);
+    }
+    public void deleteUser(User user){
+        us.delete(user);
+    }
+}
