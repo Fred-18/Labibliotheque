408f689db4f85b0f38018e53319bb579012fa487
Fred-18 <fred.nobre@octo.com>
2023-03-25 13:54:27 +0100

    :sparkles: add new author method on repository

--- src/main/java/com/labibliotheque/la_bibliotheque/repositories/AuthorRepository.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/repositories/AuthorRepository.java
@@ -3,6 +3,10 @@ import com.labibliotheque.la_bibliotheque.models.Author;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends CrudRepository<Author,Integer> {
+    List<Author> findByFirstNameAuthor(String firstname);
+    List<Author>findByLastNameAuthor(String laststname);
 }
