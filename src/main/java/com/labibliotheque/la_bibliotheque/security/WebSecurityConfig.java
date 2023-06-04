a075c7bd4d410628b63cc0e40fa75436d443b4f8
Fred-18 <fred.nobre@octo.com>
2023-06-04 15:41:52 +0200

    :lock: config web security

---
+++ src/main/java/com/labibliotheque/la_bibliotheque/security/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.labibliotheque.la_bibliotheque.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Qualifier("userService")
+    @Autowired
+    private UserDetailsService userService;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/signup", "/js/**", "/css/**").permitAll()
+                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/").defaultSuccessUrl("/dashboard").permitAll()
+                .and()
+                .logout().permitAll();
+
+        return http.build();
+    }
+
+    @Autowired
+    void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userService);
+    }
+}
+
