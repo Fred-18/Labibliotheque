c9214176a3a5fbbb902d6036bbf916c56fa6e33a
Fred-18 <fred.nobre@octo.com>
2023-06-26 09:55:31 +0200

    :sparkles: add JwtTokenFilter

---
+++ src/main/java/com/labibliotheque/la_bibliotheque/security/JwtTokenFilter.java
@@ -0,0 +1,18 @@
+package com.labibliotheque.la_bibliotheque.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Configuration
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
