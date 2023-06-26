72f4754610098bb4998308320fba53aa5f988254
Fred-18 <fred.nobre@octo.com>
2023-06-26 09:48:02 +0200

    :recycle: typo BookController

--- src/main/java/com/labibliotheque/la_bibliotheque/controllers/BookController.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/controllers/BookController.java
@@ -2,30 +2,31 @@ package com.labibliotheque.la_bibliotheque.controllers;
 
 import com.labibliotheque.la_bibliotheque.models.Book;
 import com.labibliotheque.la_bibliotheque.services.BookService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
-@RequestMapping("book")
+@RequestMapping(value = "book",produces = "application/json") //Book avec un s 
+@Tag(name="Book api")
 public class BookController {
     @Autowired
     private BookService bookService;
     
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public Book getBookById(@PathVariable("id")int id){
         return bookService.getBook(id).orElseThrow();
     }
     
-    @GetMapping("/")
+    @GetMapping()
     public Iterable<Book>getAllBooks(){
         return bookService.getAllBooks();
     }
     
-    @PostMapping("add")
+    @PostMapping()
     public void saveBook(@RequestBody Book book){
         bookService.addBook(book);
     }
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable("id")int id){
         bookService.deleteBook(id);
     }
