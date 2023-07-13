c4f39673c54c5c210803294629c1e9b9653106e0
Fred-18 <fred.nobre@octo.com>
2023-07-13 11:58:13 +0200

    :seedling: add  update PublisherController

--- src/main/java/com/labibliotheque/la_bibliotheque/controllers/PublisherController.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/controllers/PublisherController.java
@@ -2,30 +2,33 @@ package com.labibliotheque.la_bibliotheque.controllers;
 
 import com.labibliotheque.la_bibliotheque.models.Publisher;
 import com.labibliotheque.la_bibliotheque.services.PublisherService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "publishers",produces = "application/json")
+@Tag(name = "publisher api")
 public class PublisherController {
     @Autowired
     private PublisherService publisherService;
 
-    @GetMapping("/publisher/{id}")
+    @GetMapping("/{id}")
     public Publisher getPublisherById(@PathVariable int id) {
         return publisherService.getPublisher(id).orElseThrow();
     }
 
-    @GetMapping("/publishers")
+    @GetMapping()
     public Iterable<Publisher> getAllPublishers() {
         return publisherService.getAllPublishers();
     }
 
-    @PostMapping("/publisher/add")
+    @PostMapping()
     public void savePublisher(@RequestBody Publisher publisher) {
         publisherService.addPublishers(publisher);
     }
 
-    @DeleteMapping("/publisher/delete/{id}")
+    @DeleteMapping("/{id}")
     public void deletePublisher(@PathVariable("id") int id) {
         publisherService.deletePublishers(id);
     }
