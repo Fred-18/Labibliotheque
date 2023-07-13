1ba26e764429f06e3739a3e6780dad185eb963f6
Fred-18 <fred.nobre@octo.com>
2023-07-13 11:57:35 +0200

    :seedling: add  update comment controller

--- src/main/java/com/labibliotheque/la_bibliotheque/controllers/CommentController.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/controllers/CommentController.java
@@ -3,16 +3,19 @@ package com.labibliotheque.la_bibliotheque.controllers;
 import com.labibliotheque.la_bibliotheque.models.Book;
 import com.labibliotheque.la_bibliotheque.models.Comment;
 import com.labibliotheque.la_bibliotheque.services.CommentService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+    @RequestMapping(value = "comments",produces = "application/json")
+    @Tag(name = "comment api")
 public class CommentController {
     @Autowired
     private CommentService commentService;
     
-    @GetMapping("/comment/{id}")
+    @GetMapping("/{id}")
     public Comment getCommentById(@PathVariable int id){
         return commentService.getComment(id).orElseThrow();
     }
@@ -20,11 +23,11 @@ public class CommentController {
     public Iterable<Comment>getCommentsBybooks(Book book){
         return commentService.getCommentsFromBooks(book);
     }
-    @PostMapping("/comment/add")
+    @PostMapping()
     public void saveComment(@RequestBody Comment comment){
         commentService.addComment(comment);
     }
-    @PostMapping("/comment/delete/{id}")
+    @PostMapping("/{id}")
     public void deleteComment(@PathVariable("id")int id){
         commentService.deleteComment(id);
     }
