316458952667699e0405f5c39f9dfbe953d48bb2
Fred-18 <fred.nobre@octo.com>
2023-08-13 23:43:47 +0200

    :white_check_mark: add author test

--- src/main/java/com/labibliotheque/la_bibliotheque/controllers/AuthorController.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/controllers/AuthorController.java
@@ -9,33 +9,40 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "authors",produces = "application/json")
+@CrossOrigin
+@RequestMapping(value = "authors", produces = "application/json")
 @Tag(name = "author api")
 public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
     @GetMapping("/{id}")
-    public Author getAuthorById(@PathVariable("id") int id){
+    public Author getAuthorById(@PathVariable("id") int id) {
         return authorService.getAuthor(id).orElseThrow();
     }
+
     @GetMapping()
-    public Iterable<Author>getAllAuthors(){
+    public Iterable<Author> getAllAuthors() {
         return authorService.getAllAuthors();
     }
+
     @GetMapping("{firstName}")
-    public List <Author> getAuthorByFirstName(@PathVariable("firstName")String firstName){
+    public List<Author> getAuthorByFirstName(@PathVariable("firstName") String firstName) {
         return authorService.getAuthorByFirstName(firstName);
-    }@GetMapping("{lastName}")
-    public List <Author> getAuthorByLastName(@PathVariable("lastName")String lastName){
+    }
+
+    @GetMapping("{lastName}")
+    public List<Author> getAuthorByLastName(@PathVariable("lastName") String lastName) {
         return authorService.getAuthorByLastName(lastName);
     }
+
     @PostMapping()
-    public void saveAuthor(@RequestBody Author author){
+    public void saveAuthor(@RequestBody Author author) {
         authorService.addAuthor(author);
     }
+
     @DeleteMapping("/{id}")
-    public void deleteAuthor(@PathVariable("id")int id){
+    public void deleteAuthor(@PathVariable("id") int id) {
         authorService.deleteAuthor(id);
     }
 }
--- src/test/java/com/labibliotheque/la_bibliotheque/AuthorServiceTest.java
+++ src/test/java/com/labibliotheque/la_bibliotheque/AuthorServiceTest.java
@@ -1,5 +1,7 @@
 package com.labibliotheque.la_bibliotheque;
 
+import com.labibliotheque.la_bibliotheque.mappers.AuthorMapper;
+import com.labibliotheque.la_bibliotheque.models.Author;
 import com.labibliotheque.la_bibliotheque.repositories.AuthorRepository;
 import com.labibliotheque.la_bibliotheque.services.AuthorService;
 import org.junit.jupiter.api.Test;
@@ -9,15 +11,22 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
     @Mock
     public AuthorRepository repository;
+    @Mock
+    public AuthorMapper mappers;
     @InjectMocks
     public AuthorService service;
     @Test
     public void getAuthorMustCallAuthorRepoOnce(){
         service.getAuthor(1);
+
         Mockito.verify(repository,Mockito.times(1)).findById(1);
     }
 }
