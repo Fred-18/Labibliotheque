f8227d3b2e11c6f9375b4e207cdc342c1429f2be
Fred-18 <fred.nobre@octo.com>
2023-08-13 23:46:53 +0200

    :seedling: update bookmapper

--- src/main/java/com/labibliotheque/la_bibliotheque/mappers/BookMapper.java
+++ src/main/java/com/labibliotheque/la_bibliotheque/mappers/BookMapper.java
@@ -2,13 +2,13 @@ package com.labibliotheque.la_bibliotheque.mappers;
 
 import com.labibliotheque.la_bibliotheque.models.Book;
 import com.labibliotheque.la_bibliotheque.repositories.models.BookEntity;
-import org.mapstruct.MapMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel="spring")
+@Mapper(componentModel = "spring")
 public interface BookMapper {
     Book toDto(BookEntity BookEntity);
-    @Mapping(target = "publisher.id",source="idPublisher")
+
+    @Mapping(target = "publisher.id", source = "idPublisher")
     BookEntity toEntity(Book Book);
 }
